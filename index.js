2d9063fad94388a0a487bf048902b0b5e8cd1130
Phạm Tuấn Minh <tuanminh100217@gmail.com>
2025-03-31 16:18:13 +0700

    cai dat morgan

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var morgan = require('morgan')
+
+app.use(morgan('combined'))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
